447751337e5f67d31b5b4cd9338e0cc6d1a9d9cf
Priyanshika Sharma <adipri2003@gmail.com>
2022-11-15 23:07:34 +0530

    commit message

---
+++ src/team/TeamData.js
@@ -0,0 +1,33 @@
+import Pratima from './image/Pratima.png';
+import Purnima from './image/Purnima.jpg';
+import Pri from './image/Pri.jpg';
+
+const TeamMemberDescription = 'Galgotias College of Engg & Technology';
+
+export const TeamData = [
+  {
+    img: Pri,
+    name: 'Priyanshika Sharma',
+    description: TeamMemberDescription,
+    linkedInUrl: 'https://www.linkedin.com/in/priyanshika',
+    gitHubUrl: 'https://github.com/Aditig2020',
+  },
+  {
+    img: Purnima,
+    name: 'Purnima Katiyar',
+    description: TeamMemberDescription,
+    linkedInUrl: 'https://www.linkedin.com/in/purnima-katiyar-6a1a92134',
+    gitHubUrl: 'https://github.com/purnimakatiyar',
+  },
+  {
+    img: Pratima,
+    name: 'Pratima Kumari',
+    description: TeamMemberDescription,
+    linkedInUrl: 'https://www.linkedin.com/in/pratima-kumari-8712b3227',
+    gitHubUrl: '',
+  },
+];
+export const TeamDescription =
+  'We are a group of highly motivated students from Galgotias ' +
+  'College of Engineering & Technology that are invested in the field of Computer Science Engineering.' +
+  ' Feel free to contact us.';
